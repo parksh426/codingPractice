befe8cfb40ae64a7efaf0af08e1a9f57ce812b9d
LG GRAM13 <LG GRAM13@LGGRAM13>
2020-03-22 23:39:19 +0900

    Greedy2 중간커밋

--- src/main/GreedyProblem2.java
+++ src/main/GreedyProblem2.java
@@ -44,13 +44,138 @@ return
 
 	 */
 	
-	public GreedyProblem2() {
-		
-	}
 
 	public int solution(String name) {
+		System.out.println("solution started: " + name);
 		int answer = 0;
+		
+		int oldIndex = 0;
+		int index = 0;
+		int nextIndex = 0;
+		char c;
+		int a, b;
+		char[] letters = new char[name.length()];
+		for(int i=0; i<letters.length; i++) {
+			letters[i] = 'A';
+		}
+		
+		boolean ignore = false;
+		
+		while(nextIndex < name.length()) {
+			nextIndex = getNextIndex(index, name);
+			if(letters[nextIndex] == name.charAt(nextIndex)) {
+				break;
+			}
+			System.out.println(String.format("next index:%d", nextIndex));
+			//a = (nextIndex-oldIndex);
+			a = (nextIndex);
+			b = (name.length()-nextIndex+oldIndex);
+			if(!ignore) {
+				//int nonA = nonACountRight(name, nextIndex);
+				if(a < b) {
+					answer += a-oldIndex;
+				}
+				// AABA, JEROEN, BBAABBB, BAAAABAA, BBBBBABA (a=6, b=8-6+4), BAAABA (a=4, b=6-4+0)
+				else if(a == b) {
+					if(name.charAt(name.length()-1) == 'A' || oldIndex+1 == nextIndex) {
+						answer += a-oldIndex;
+					}
+					else {
+						answer += b;
+					}
+				}
+				else {
+					answer += b;
+				}
+			}
+			if(a >= b) {
+				ignore = true;
+			}
+			System.out.println(String.format("position shift:%d", a < b ? a-oldIndex : b));
+			
+			c = name.charAt(nextIndex);
+			a = (c-'A');
+			b = ('Z'-c+1);
+			answer += a < b ? a : b;
+			System.out.println(String.format("alphabet change:%d", a < b ? a : b));
+			letters[nextIndex] = c;
+			
+			oldIndex = nextIndex;
+			// move to right
+			index = nextIndex + 1;
+			if(index == name.length()) {
+				index = 0;
+			}
+		}
+		
+		
 		return answer;
 	}
+	
+	public int nonACountRight(String name, int nextIndex) {
+		int result = 0;
+		for(int i=name.length()-1; i>=nextIndex; i--) {
+			if(name.charAt(i) == 'A') {
+				return name.length()-i;
+			}
+		}
+		
+		return result;
+	}
+	
+	public int getNextIndex(int current, String name) {
+		int result = 0;
+		
+		// move to right
+		for(result=current; result<name.length(); result++) {
+			if(name.charAt(result) != 'A') {
+				return result;
+			}
+		}
+		
+		for(result=0; result<current; result++) {
+			if(name.charAt(result) != 'A') {
+				return result;
+			}
+		}
+		
+		return result;
+	}
+	
+	
+
+	public int[] findBeginningIndex(String name) {
+		int[] result = new int[2]; // [0] beginning index, [1] 1:right, 2:left
+		
+		int start=0, end=name.length()-1;
+		if(name.charAt(0) != 'A') {
+			// BBBAAB
+			// BBAAAAAAABB
+			for(int i=0; i<name.length(); i++) {
+				if(name.charAt(i) != 'A') {
+					end = i-1;
+					break;
+				}
+			}
+			
+			for(int i=name.length()-1; i>=0; i--) {
+				if(name.charAt(i) != 'A') {
+					start = i;
+//					if(start == name.length()) {
+//						start = 0;
+//					}
+				}
+			}
+			
+			if(name.length()-1-start < end) {
+				
+			}
+		}
+		
+		//AAAAAAABBBBBBBBBBBBBAA
+		
+		return result;
+	}
+	
 
 }
--- src/test/GreedyProblem2Test.java
+++ src/test/GreedyProblem2Test.java
@@ -9,7 +9,7 @@ import main.GreedyProblem2;
 public class GreedyProblem2Test {
 
 	@Test
-	public void testSolution() {
+	public void testSolution00() {
 		GreedyProblem2 greedy2 = new GreedyProblem2();
 		int result = greedy2.solution("JEROEN");
 		int expected = 56;
@@ -18,12 +18,174 @@ public class GreedyProblem2Test {
 	}
 	
 	@Test
-	public void testSolution2() {
+	public void testSolution000() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("BBBBBABA");
+		int expected = 12;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution001() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("AAABAA");
+		int expected = 4;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution002() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("AAABA");
+		int expected = 3;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution003() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("AAAAA");
+		int expected = 0;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution004() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("BAAAABAA");
+		int expected = 5;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution005() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("BBBBBABA");
+		int expected = 12;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution006() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("BAAABA");
+		int expected = 4;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution01() {
 		GreedyProblem2 greedy2 = new GreedyProblem2();
 		int result = greedy2.solution("JAN");
 		int expected = 23;
 		
 		assertEquals(expected, result);
 	}
-
+	
+	@Test
+	public void testSolution02() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("AAA");
+		int expected = 0;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution03() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("AAZ");
+		int expected = 2;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution04() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("ZAA");
+		int expected = 1;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution05() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("BBABA");
+		int expected = 6;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution06() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("AAAAAAAAAABABB");
+		int expected = 7;
+		
+		assertEquals(expected, result);
+	}
+	
+//	@Test
+//	public void testSolution06() {
+//		GreedyProblem2 greedy2 = new GreedyProblem2();
+//		int result = greedy2.solution("BBBAAB");
+//		int expected = 8;
+//		
+//		assertEquals(expected, result);
+//	}
+//	
+	@Test
+	public void testSolution07() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("BBAABAA");
+		int expected = 7;
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testSolution08() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("BBAABBB");
+		int expected = 10;
+		
+		assertEquals(expected, result);
+	}
+//	
+//	@Test
+//	public void testSolution09() {
+//		GreedyProblem2 greedy2 = new GreedyProblem2();
+//		int result = greedy2.solution("BBAABAAAA");
+//		int expected = 7;
+//		
+//		assertEquals(expected, result);
+//	}
+//	
+//	@Test
+//	public void testSolution10() {
+//		GreedyProblem2 greedy2 = new GreedyProblem2();
+//		int result = greedy2.solution("BBAABAAAAB");
+//		int expected = 10;
+//		
+//		assertEquals(expected, result);
+//	}
+	
+	@Test
+	public void testSolution11() {
+		GreedyProblem2 greedy2 = new GreedyProblem2();
+		int result = greedy2.solution("BBBBAAAAAAABBBA");
+		int expected = 17;
+		
+		assertEquals(expected, result);
+	}
+	
 }
